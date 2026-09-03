101039e253ba62a35a340560f6e5e73162b48931
faturrahman <zexafatur@gmail.com>
2026-09-03 07:12:11 +0700

    Initial library OOP project

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+<?php
 
+require_once "Book.php";
+require_once "DigitalBook.php";
 
+$book = new Book(
+    "The Great Gatsby",
+    "F. Scott Fitzgerald"
+);
 
+echo $book->getInfo();
+echo "\n";
+echo $book->borrow();
+
+echo "\n\n";
+
+$digitalBook = new DigitalBook(
+    "The Great Gatsby",
+    "F. Scott Fitzgerald"
+);
+
+echo $digitalBook->getInfo();
+echo "\n";
+echo $digitalBook->download();
