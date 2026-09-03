d27a5c446718ba75eaee078c67119644090cb689
faturrahman <zexafatur@gmail.com>
2026-09-03 07:10:25 +0700

    Add DigitalBook inheritance

--- DigitalBook.php
+++ DigitalBook.php
@@ -9,26 +9,3 @@ class DigitalBook extends Book
         return "Digital book downloaded: " . $this->title;
     }
 }
-
-
-
-
-$book = new Book(
-    "The Great Gatsby",
-    "F. Scott Fitzgerald"
-);
-
-echo $book->getInfo();
-echo "\n";
-echo $book->borrow();
-
-echo "\n\n";
-
-$digitalBook = new DigitalBook(
-    "The Great Gatsby",
-    "F. Scott Fitzgerald"
-);
-
-echo $digitalBook->getInfo();
-echo "\n";
-echo $digitalBook->download();
